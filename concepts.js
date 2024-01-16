0f39bfa4db2594b597d67f32059d1eca810cdf6f
kishoren99 <kishorenagarajachar@gmail.com>
2024-01-16 20:46:16 +0530

    a1

---
+++ concepts.js
@@ -0,0 +1,191 @@
+// console.log(a);
+// var a=29;
+// console.log(a);
+// var a=3;
+// console.log(a);
+
+// let b="HELLO I AM KISHORE "
+// console.log(b);
+// // let b=34;
+
+// let a=10/3
+// console.log(typeof a);
+// console.log(typeof(a.toString()));
+// let b=a.toString()
+// console.log(b);
+// console.log(typeof b);
+
+// const c=9
+// console.log(c);
+
+// const d=11;
+// console.log(d);
+
+
+// console.log("HI EVERYONE");
+// let str='   HElloooo MAVA  '
+// console.log(str);
+// console.log(str.length);
+// console.log(str.trimStart().length)
+// console.log(str.trimStart());
+// console.log(str.trimEnd());                                     
+// console.log(str.trimEnd().length);
+//   str=str.trim();
+
+// console.log(str.slice(1,5))
+// console.log(str.toUpperCase());
+// console.log(str.slice(-3,-1));
+
+// var if=445;
+// console.log(if);
+
+// var a="hello Everyone"
+// let b=2;
+// console.log(a);
+// console.log(typeof a);
+// console.log(b);
+// console.log(typeof b)
+// console.log(a-b);
+// console.log(a+" "+b);
+// console.log(typeof(a+" "+ b));
+
+// var x=10;
+// let y=3;
+// let z=x/y
+// console.log(z); 
+// console.log(parseInt(z));
+// console.log(parseFloat(z));
+
+// let num=12345678;
+// let count=0
+// while (num!=0) {
+//     num=num%10;
+//     count++;
+// }
+// console.log(num);
+// console.log("hi");
+// var str="HELLO MAN AND WOMAN";
+// console.log(str.length);
+
+// console.log(10-'hello');
+// console.log(10*'hiii');
+// console.log(typeof(10-"hii"));
+// console.log(10+true);
+// console.log(Number (true));
+
+
+// console.log(window);
+// let a=25;
+// a=10
+// console.log(a);
+
+// console.log(45-"hiii");
+// var a=Number('helloo')
+// console.log(a);
+
+
+// var s='Hello machha how are You'
+// console.log(s);
+// console.log(s.length);
+// console.log(s[6]);
+// console.log(s.indexOf('u'));
+// console.log(s[s.length-1]);
+// console.log(s.charAt(6));
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+// console.log(s.charCodeAt(8));
+// console.log(s.charCodeAt(98));
+// console.log( s.indexOf('o',6));
+// document.write(s.indexOf('h'))
+
+// let mail='     kishorenagarajachar@gmail.com  ';
+// console.log(mail.startsWith('k'));
+// console.log(mail.endsWith('com'));
+// console.log(mail.endsWith('f'))
+// console.log(typeof(mail.includes('kiss')));
+
+// console.log(mail.length);
+// console.log(mail.trim('').length);
+// // console.log(mail);
+// console.log(mail.trimStart('').length);
+// console.log(mail.trimEnd('').length);
+
+
+// let str='kishorenagarajachar@gmail.com'
+// console.log(str.slice(15));
+// console.log(str.slice(3,4));
+// console.log(str[1]);
+// console.log(str.slice(-15,-8));
+// console.log(str.slice(-4,-1));
+// console.log(str.slice(-1,4));
+
+// console.log(str.substring(-1,4));//if we pass the negative value it will consider it as zero
+// console.log(str.substring(9,0));
+// console.log(str.substring(5,2));
+// console.log(str.substring(455,0));
+
+// // substr//
+// console.log(str.substr(4,9));
+
+// let x='hello sun weds moon'
+// console.log(x.replace('sun' , 'earth'));
+// console.log(x.replaceAll('moon', 'noon'));
+
+// var x='Abhilash Sir is a good boy'
+// console.log(x);
+
+// var c=console.log(x.split(''));
+// console.log(x.split(' '));
+
+// console.log(x.split(' ').reverse().join(' '));
+
+// console.log(x);
+// const x=39
+// console.log("hiiiii");
+
+// console.log(String(10 == 9));
+// console.log(Boolean(10/3));
+
+// big int
+// let a=BigInt (12)
+// console.log(a);
+
+// let b=122n
+// console.log( b);
+
+// let c=123
+// console.log(a+b+c);
+
+// var a='12'
+// let b=12
+// console.log(a===b);
+
+// var v=prompt("enter the first number")
+// var v1=prompt('enter the second number')
+// console.log(v*v1);
+
+
+// console.log(window);
+
+var k=56
+function hii(x, y ,z) {
+    var a=x
+    let b=y
+    // let b=hiiimavav
+    const c=z
+    // c=544
+    console.log(a+b+c);
+    console.log(k);
+}
+function subfun(a,b){
+    console.log(a+b);
+}
+hii(subfun('hello ' , 'may i come in '))
+hii(52,48,69)
+
+// console.log(a);
+
+
+var arr=['hello',21,null,true,undefined,34n,()=>console.log("i am arrow function");] {
+    
+}]
